bf6a1daf973f99878712144a870d8a422f56c6cd
MANUEL RICO <rico31858@gmail.com>
2026-04-23 12:17:46 -0600

    Se estan trabajando en pequenios errores que surgieron y se esta buscando como corregir y como se implementara la solucion

--- app/src/main/java/com/example/maps/data/RouteModels.kt
+++ app/src/main/java/com/example/maps/data/RouteModels.kt
@@ -1,14 +1,12 @@
 package com.example.maps.data
 
-// Esta clase representa el JSON completo que responde la API
+// Esta clase representa el JSON completo que responde la API para usarlo
 data class RouteResponse(
     val routes: List<Route>
 )
-
 data class Route(
     val overview_polyline: PolylineData
 )
-
 data class PolylineData(
     val points: String
 )
